49312e47382b2be20174dcc5eb15ea345592f592
aniket00101 <dasaniket971@gmail.com>
2025-07-20 13:01:21 +0530

    few changes

--- src/components/navbar/Navbarlogo.jsx
+++ src/components/navbar/Navbarlogo.jsx
@@ -4,10 +4,10 @@ function Navbarlogo() {
     return (
         <div>
             <h1 className="text-white text-2xl sm:hidden md:block">
-                Aniket Das
+                Aniket Das Portfolio
             </h1>
             <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
-                AD
+                Aniket
             </h1>
         </div>
     )
