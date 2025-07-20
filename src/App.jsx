79ff27e9a41af0172fe71642c73bb8441b9e3af6
aniket00101 <dasaniket971@gmail.com>
2025-07-20 11:56:32 +0530

    border

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,27 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 3659);
+    }, 3655);
     return () => clearTimeout(timer);
   }, []);
   return (
-    <main className="font-body min-h-screen">
+    <main className="font-body min-h-screen ">
       {loading ? (
         <div className="grid place-items-center min-h-screen w-screen bg-[#121621]">
-      <HamsterLoader />
-    </div>
-      ) : (
-        <div className="pt-3">
-          <Navbarmain />
-          <HeroMain />
+          <HamsterLoader />
         </div>
+      ) : (
+        // <div className="pt-3 w-screen mx-1">
+        //   <Navbarmain />
+        //   <HeroMain />
+        // </div>
+        <div className="fixed inset-0 flex items-center justify-center bg-black">
+  <div className="w-[98%] h-[95vh] m-2 p-2 border border-white box-border">
+    <Navbarmain />
+    <HeroMain />
+  </div>
+</div>
+
       )}
     </main>
   );
