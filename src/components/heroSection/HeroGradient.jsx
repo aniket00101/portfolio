60298c9e84521fb2c5c14eb474433b61411d3f00
aniket00101 <dasaniket971@gmail.com>
2025-07-26 23:03:16 +0530

    few commit

--- src/components/heroSection/HeroGradient.jsx
+++ src/components/heroSection/HeroGradient.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 export const HeroGradient = () => {
   return (
     <div>
-        <div className="shadow-cyanMediumShadow absolute top-[300px] right-0 animate-pulse opacity-65"></div>
+        <div className="shadow-cyanMediumShadow absolute top-[450px] right-0 animate-pulse opacity-65"></div>
 
         <div className="shadow-cyanMediumShadow absolute top-[700px] right-0 animate-pulse opacity-65"></div>
 
         <div className="shadow-cyanMediumShadow absolute top-0 left-0 opacity-65"></div>
 
-        <div className="shadow-cyanMediumShadow absolute top-[300px] left-0 opacity-65 animate-pulse"></div>
+        <div className="shadow-cyanMediumShadow absolute top-[250px] left-0 opacity-65 animate-pulse"></div>
     </div>
   )
 }
