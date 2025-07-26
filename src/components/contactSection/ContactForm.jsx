0835db2e5339540951cd32ff492205a8fc06e884
aniket00101 <dasaniket971@gmail.com>
2025-07-26 22:39:00 +0530

    few bug fixes

--- src/components/contactSection/ContactForm.jsx
+++ src/components/contactSection/ContactForm.jsx
@@ -41,9 +41,13 @@ const ContactForm = () => {
     <div>
       <p className="text-cyan">{success}</p>
         <form action="" className="flex flex-col gap-4 text-white mt-6" ref={form} onSubmit={sendEmail}>
-            <input type="text" name="from_name" placeholder="Your Name" required className="h-12 rounded-lg bg-black/10 px-2 border-b-2 border-orange bg-transparent focus:outline-none focus:border-orange" value={name} onChange={handleName}/>
-            <input type="email" name="from_email" placeholder="Your Email" required className="h-12 rounded-lg bg-black/10 px-2 border-b-2 border-orange bg-transparent focus:outline-none focus:border-orange" value={email} onChange={handleEmail}/>
-            <textarea type="text" name="messages" placeholder="Message" rows='9' cols='50' required className="rounded-lg bg-black/10 border-b-2 p-2 border-orange bg-transparent focus:outline-none" value={messages} onChange={handleMessages}/>
+
+            <input type="text" name="from_name" placeholder="Your Name" required className="h-12 bg-black/5 px-2 border-b-2 rounded-lg border-b-orange focus:outline-none focus:border-orange border-l-2 border-l-orange " value={name} onChange={handleName}/>
+
+            <input type="email" name="from_email" placeholder="Your Email" required className="h-12 rounded-lg bg-black/5 px-2 border-b-2 border-orange bg-transparent focus:outline-none focus:border-orange border-l-2 border-l-orange" value={email} onChange={handleEmail}/>
+
+            <textarea type="text" name="messages" placeholder="Message" rows='9' cols='50' required className="rounded-lg bg-black/10 border-b-2  p-2 border-orange bg-transparent border-l-2 border-l-orange focus:outline-none" value={messages} onChange={handleMessages}/>
+
             <button type="submit" className="w-full rounded-lg border  text-white h-12 font-bold text-xl hover:bg-orange/50 bg-orange/80 transition-all duration-500">Send</button>
         </form>
     </div>
@@ -51,3 +55,4 @@ const ContactForm = () => {
 }
 
 export default ContactForm
+
--- src/components/contactSection/ContactMeMain.jsx
+++ src/components/contactSection/ContactMeMain.jsx
@@ -6,7 +6,7 @@ const ContactMeMain = () => {
   return (
     <div id="contact" className="max-w-[1200px] mx-auto items-center justify-center mt-[100px] px-4">
         <h2 className="text-6xl text-cyan mb-10 text-center">Contact Me</h2>
-        <div className="flex justify-between gap-24 border-[2px] backdrop-blur-md border-orange p-8 rounded-2xl lg:flex-row sm:flex-col">
+        <div className="flex justify-between gap-24 border-[2px] backdrop-blur-[30px] border-orange p-5 rounded-2xl lg:flex-row sm:flex-col">
             <ContactMeleft />
             <ContactMeright />
         </div>
