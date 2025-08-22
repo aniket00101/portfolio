5cbd8bc6ebd67b15b4eb20c6616bfe68ddb16a0f
aniket00101 <dasaniket971@gmail.com>
2025-08-22 23:59:29 +0530

    mini change

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -6,12 +6,9 @@ import { PiHexagonThin } from "react-icons/pi"
 export const HeroPic = () => {
   return (
     <motion.div variants={fadeIn('left',0.2)} initial = "hidden" whileInView="show" viewport={{ once: false, amount: 0}} className="h-full relative flex items-center justify-center overflow-hidden">
-      {/* Blurred spinning hexagon - absolute, non-impacting layout */}
       <div className="absolute -z-10 flex justify-center items-center inset-0">
         <PiHexagonThin className="h-[420px] w-[420px] text-cyan blur-md animate-[spin_20s_linear_infinite]" />
       </div>
-
-      {/* Your main image */}
       <img
         src="/images/myself/me.png"
         alt="Aniket Das"
--- src/components/heroSection/HeroText.jsx
+++ src/components/heroSection/HeroText.jsx
@@ -6,7 +6,7 @@ export const HeroText = () => {
     return (
         <div className="flex flex-col gap-4 h-full justify-center md:text-left md:pl-10 sm:text-center">
             <motion.h2 variants={fadeIn('down',0.2)} initial = "hidden" whileInView="show" viewport={{ once: false, amount: 0}}
-            className="lg:text-3xl font-bold sm:text-xl uppercase text-cyan">MERN stack Developer</motion.h2>
+            className="lg:text-6xl font-bold sm:text-xl uppercase text-cyan">MERN stack Developer</motion.h2>
 
             <motion.h1 variants={fadeIn('right',0.4)} initial = "hidden" whileInView="show" viewport={{ once: false, amount: 0}} className="md:text-[2.8rem] lg:text-6xl sm:text-4xl font-bold font-special text-orange">Aniket Das</motion.h1>
 
@@ -14,3 +14,4 @@ export const HeroText = () => {
         </div>
     )
 }
+
