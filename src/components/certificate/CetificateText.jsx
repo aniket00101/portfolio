0d74c535ddf6758d83b29a4d47316a518e39a21b
aniket00101 <dasaniket971@gmail.com>
2025-08-23 21:03:40 +0530

    added

--- src/components/certificate/CetificateText.jsx
+++ src/components/certificate/CetificateText.jsx
@@ -4,7 +4,7 @@ const CetificateText = () => {
   return (
     <div className="flex flex-col items-center mt-[100px]">
         <h2 className="text-6xl text-cyan md-10">Certificates</h2>
-        <p className="text-2xl text-center text-white mt-[15px]">I have successfully earned multiple certifications that not only demonstrate my expertise in web development, programming, and software engineering but also reflect my continuous commitment to personal and professional growth. These certifications represent both academic achievements and industry-recognized credentials, showcasing my ability to adapt to evolving technologies and apply my skills in real-world scenarios.</p>
+        <p className="sm:text-lg lg:text-2xl text-center text-white mt-[15px]">I have successfully earned multiple certifications that not only demonstrate my expertise in web development, programming, and software engineering but also reflect my continuous commitment to personal and professional growth. These certifications represent both academic achievements and industry-recognized credentials, showcasing my ability to adapt to evolving technologies and apply my skills in real-world scenarios.</p>
     </div>
   )
 }
