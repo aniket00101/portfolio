a51d6ef83f8ab432c796b692805da0d4fa55f633
Aniket Das <dasaniket971@gmail.com>
2026-02-17 12:14:55 +0530

    Update GitHub link in Contactsocial component

--- src/components/SocialLinks/Contactsocial.jsx
+++ src/components/SocialLinks/Contactsocial.jsx
@@ -7,7 +7,7 @@ import { SiLeetcode } from "react-icons/si";
 const Contactsocial = () => {
   return (
     <div className="flex gap-4">
-        <SingleContactSocial link='https://github.com/Build-With-Aniket-56' Icon={PiGithubLogoBold}/>
+        <SingleContactSocial link='https://github.com/aniket00101' Icon={PiGithubLogoBold}/>
         <SingleContactSocial link='https://www.linkedin.com/in/aniket-das-7766b129a/' Icon={FaLinkedinIn}/>
         <SingleContactSocial link='https://www.instagram.com/_aniketdas_?igsh=bHNuNTI4OW0yNG9t' Icon={FaInstagram}/>
     </div>
