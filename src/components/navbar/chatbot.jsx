7b1e85de7d25ef6f2be4066152235b010ea865a8
Aniket Das <dasaniket971@gmail.com>
2026-03-18 00:20:09 +0530

    Update chatbot.jsx

--- src/components/navbar/chatbot.jsx
+++ src/components/navbar/chatbot.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { FaRobot, FaUserLarge } from "react-icons/fa6";
 
-const API_URL = "https://aniket-chatbot-api.onrender.com";
+const API_URL = "https://aniket-portfolio-chatbot.onrender.com";
 
 function Chatbot() {
   const [messages, setMessages] = useState([
